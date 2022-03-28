dc63f20d939da9fad6efb9e22d6b0f851c8e1600
AhmedA00 <aabed10x@gmail.com>
2022-03-29 00:10:50 +0300

    Committing Login Function and Design

--- HMS/accounts/urls.py
+++ HMS/accounts/urls.py
@@ -4,8 +4,8 @@ from .import views
 urlpatterns=[
     path('create_patients/',views.createPatient),
     path('patient/<str:pk>/',views.patient),
-    path('register/',views.registerPage)
-
+    path('register/',views.registerPage),
+    path('login/',views.loginPage)
 
 
 
--- HMS/accounts/views.py
+++ HMS/accounts/views.py
@@ -7,6 +7,8 @@ from .forms import PatientForm, CreateUserForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
+from django.contrib.auth import authenticate,login,logout
+
 # Create your views here.
 
 def registerPage(request):
@@ -23,6 +25,23 @@ def registerPage(request):
 
     context={'form':form}
     return render(request,'accounts/register.html',context)
+
+def loginPage(request):
+
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,'Username or Password is incorrect')
+
+    context={}
+    return render(request,'accounts/login.html',context)
 
 
 def patient (request,pk):
