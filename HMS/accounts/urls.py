818899245466207e108461d90ef4346c0c41d6f2
AhmedA00 <aabed10x@gmail.com>
2022-03-29 01:21:03 +0300

    Committing Main Page Features Trial

--- HMS/accounts/urls.py
+++ HMS/accounts/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
+    path('', views.home, name="home"),
     path('create_patients/',views.createPatient),
     path('patient/<str:pk>/',views.patient),
-    path('register/',views.registerPage),
-    path('login/',views.loginPage),
-    path('logout/',views.logoutUser)
+    path('register/',views.registerPage,name="register"),
+    path('login/',views.loginPage,name="login"),
+    path('logout/',views.logoutUser,name="logout")
 
 
 
--- HMS/accounts/views.py
+++ HMS/accounts/views.py
@@ -7,64 +7,91 @@ from .forms import PatientForm, CreateUserForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 def registerPage(request):
-    form= CreateUserForm()
+    form = CreateUserForm()
+    if request.user.is_authenticated():
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')  # To get the username created
+                messages.success(request, 'Account was created for ' + user)  # Message to pop up when registered
 
-    if request.method =='POST':
-        form=UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user=form.cleaned_data.get('username')#To get the username created
-            messages.success(request,'Account was created for '+ user)#Message to pop up when registered
+                return redirect('login')
 
-            return redirect('login')
+        context = {'form': form}
+        return render(request, 'accounts/register.html', context)
 
-    context={'form':form}
-    return render(request,'accounts/register.html',context)
 
 def loginPage(request):
-#Login function to get in the system
-    if request.method=='POST':
-        username=request.POST.get('username')
-        password=request.POST.get('password')
+    # Login function to get in the system
+    if request.user.is_authenticated():
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user=authenticate(request,username=username,password=password)
+            user = authenticate(request, username=username, password=password)
 
-            #Making sure that the user is registered and correct
-        if user is not None:
-            login(request,user)
-            return redirect('home')
-            #If the information was incorrect a message will show that
-        else:
-            messages.info(request,'Username or Password is incorrect')
+            # Making sure that the user is registered and correct
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+                # If the information was incorrect a message will show that
+            else:
+                messages.info(request, 'Username or Password is incorrect')
 
-    context={}
-    return render(request,'accounts/login.html',context)
+        context = {}
+        return render(request, 'accounts/login.html', context)
 
-def logoutUser (request):
-#Logout function that redirects the user to the login page
+
+def logoutUser(request):
+    # Logout function that redirects the user to the login page
     logout(request)
     return redirect('login')
 
 @login_required(login_url='login')
-def patient (request,pk):
-    patient=Patient.objects.get(id=pk)
-    appointments=patient.order_set.all()
-    appointment_count=appointments.count
+def home(request):
+	appointments = Appointment.objects.all()
+	patients = Patient.objects.all()
 
-    context={'patient':patient,'appointments':appointments,
-             'appointment_count':appointment_count}
-    return render(request,'accounts/patient.html',context)
+	total_patients = patients.count()
+
+	total_appointmets = appointments.count()
+
+	pending = appointments.filter(status='Pending').count()
+
+	context = {'appointments':appointments, 'patients':patients,
+	'total_appointmets':total_appointmets,'pending':pending }
+
+	return render(request, 'accounts/dashboard.html', context)
+
+
 
 @login_required(login_url='login')
-def createPatient (request):
-# To render the patient form and edit on it
-    form=PatientForm()
+def patient(request, pk):
+    patient = Patient.objects.get(id=pk)
+    appointments = patient.order_set.all()
+    appointment_count = appointments.count
 
-    context={'form':form}
-    return render(request,'accounts/patient_form',context)
+    context = {'patient': patient, 'appointments': appointments,
+               'appointment_count': appointment_count}
+    return render(request, 'accounts/patient.html', context)
+
+
+@login_required(login_url='login')
+def createPatient(request):
+    # To render the patient form and edit on it
+    form = PatientForm()
+
+    context = {'form': form}
+    return render(request, 'accounts/patient_form', context)
